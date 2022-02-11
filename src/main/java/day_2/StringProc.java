93178b6838c94785bf27fbfd1743e2d356cbe4cf
KateLis84 <emma12022002@gmail.com>
2022-02-11 15:00:29 +0200

    Task 2

---
+++ src/main/java/day_2/StringProc.java
@@ -0,0 +1,9 @@
+package day_2;
+
+public class StringProc {
+    public static void main(String[] args) {
+        String words = "Hello world";
+
+        System.out.println(words.split(" ").length);
+    }
+}
