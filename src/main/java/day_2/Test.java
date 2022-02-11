d84ca4b756235f4833c23c8a4ff75ba00f58adc8
KateLis84 <emma12022002@gmail.com>
2022-02-11 14:55:41 +0200

    Task1

--- src/main/java/day_2/Test.java
+++ src/main/java/day_2/Test.java
@@ -2,9 +2,29 @@ package day_2;
 
 public class Test {
     public static void main(String[] args){
-        System.out.println("hello");
+        double a = -2.666;
+        double b = 10;
+
+        System.out.println("a+b=" + add(a, b));
+        System.out.println("a-b=" + minus(a, b));
+        System.out.println("a*b=" + mult(a, b));
+        System.out.println("a/b=" + devide(a, b));
     }
 
+    public static double add(double a, double b){
+        return a+b;
+    }
 
+    public static double minus(double a, double b){
+        return a-b;
+    }
+
+    public static double mult(double a, double b){
+        return a*b;
+    }
+
+    public static double devide(double a, double b){
+        return a/b;
+    }
 
 }
