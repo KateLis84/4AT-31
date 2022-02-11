4af4a3ffc254a3c7bc17004380ccecbd1b16f133
KateLis84 <emma12022002@gmail.com>
2022-02-11 15:19:44 +0200

    Task 4
    Finished the task and added a little bit more words to text

--- src/main/java/day_2/StringProc.java
+++ src/main/java/day_2/StringProc.java
@@ -3,13 +3,20 @@ package day_2;
 public class StringProc {
     public static void main(String[] args) {
         //Task 2
-        String words = "Hello world";
+        String words = "Some random text in which I have some words :)";
         System.out.println("Number of words: " + words.split(" ").length);
-
+        String[] array = words.split(" ");
         //Task 3
-        System.out.println("Lenght: " + "\t");
-        for(String w:words.split(" ")){
+        System.out.println("Lenght of each word: " + "\t");
+        for(String w:array){
             System.out.println(w.length()+"\t"+w);
         }
+
+        //Task 4
+        System.out.println("Length of word \"random\": "+wordCount(array, 1));
+    }
+
+    private static int wordCount(String[] words, int i) {
+        return words[i].length();
     }
 }
