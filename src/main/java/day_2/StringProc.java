1eaaffa9b285ba767a31fa91faccf2f804984864
KateLis84 <emma12022002@gmail.com>
2022-02-11 15:08:54 +0200

    Task 3

--- src/main/java/day_2/StringProc.java
+++ src/main/java/day_2/StringProc.java
@@ -2,8 +2,14 @@ package day_2;
 
 public class StringProc {
     public static void main(String[] args) {
+        //Task 2
         String words = "Hello world";
+        System.out.println("Number of words: " + words.split(" ").length);
 
-        System.out.println(words.split(" ").length);
+        //Task 3
+        System.out.println("Lenght: " + "\t");
+        for(String w:words.split(" ")){
+            System.out.println(w.length()+"\t"+w);
+        }
     }
 }
