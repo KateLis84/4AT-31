5b7f86f89e5004dd3f758dbcaec40ed5eddb5dd7
KateLis84 <emma12022002@gmail.com>
2022-06-02 17:29:44 +0300

    api tests

--- src/test/java/itstep_aqa/Framework/MainTest.java
+++ src/test/java/itstep_aqa/Framework/MainTest.java
@@ -8,15 +8,20 @@ import day_12.BrowserFactory;
 import day_13.CustomSuiteListener;
 import day_13.CustomTestListener;
 import day_14.AllureTestListener;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
@@ -24,13 +29,11 @@ import java.time.temporal.ChronoUnit;
 @Listeners({
         CustomTestListener.class,
         CustomSuiteListener.class,
-        UniversalVideoListener.class,
+        //UniversalVideoListener.class,
         AllureTestListener.class
 })
 public class MainTest extends BaseUITest {
     private ThreadLocal<WebDriver> driver = new InheritableThreadLocal<>();
-
-
     @Test
     @Video
     void LoginCatlifeTest() {
@@ -52,7 +55,6 @@ public class MainTest extends BaseUITest {
         // Step 4 put Login
         catLifeBO.putLogin("kate_newAT", "12022002kate");
     }
-
     @Test
     void SearchCategoryTest() {
         driver.set(new ChromeDriver());
@@ -76,7 +78,6 @@ public class MainTest extends BaseUITest {
         //Step 7 varify search
         catLifeBO.verifyGanre();
     }
-
     @DataProvider()
     public static Object[][] SearchDataProvider() {
         return new Object[][]{
@@ -85,7 +86,6 @@ public class MainTest extends BaseUITest {
                 {"[hgpegwgjdsjicjcnvue"}
         };
     }
-
     @Test(dataProvider = "SearchDataProvider")
     void SearchWattpadTest(String searchWord) {
         driver.set(new ChromeDriver());
@@ -156,10 +156,50 @@ public class MainTest extends BaseUITest {
         //Step 10 Delete the Book from library
         catLifeBO.DeleteBook();
     }
-
     @AfterTest
     void CloseDriver(){
         driver.get().quit();
         //driver.quit();
+    }
+
+    @Test
+    void apiGetTest(){
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println("Response body: " + response.getBody().asString());
+        Assert.assertEquals(response.getStatusCode(), 200);
+
+    }
+    @Test
+    void apiPostTest(){
+        Response response = given().when().contentType(ContentType.JSON)
+                        .with().body("{\"name\": \"Kate\",\"job\": \"developer\"\n" +"}")
+                        .when().post("https://reqres.in/api/users");
+
+        response.then().assertThat().statusCode(201);
+        System.out.println("Response: " + response.getBody().asString());
+    }
+    @Test
+    void apiPutTest(){
+        Response response = given().when().contentType(ContentType.JSON)
+                .with().body("{\"name\": \"Kate\",\"job\": \"new_job\"\n" +"}")
+                .when().put("https://reqres.in/api/users/2");
+
+        response.then().assertThat().statusCode(200);
+        System.out.println("Response: " + response.getBody().asString());
+    }
+    @Test
+    void apiDeleteTest(){
+        Response response = given().when().contentType(ContentType.JSON)
+                .when().delete("https://reqres.in/api/users/2");
+
+        response.then().assertThat().statusCode(204);
+        System.out.println("Response: " + response.getBody().asString());
+        System.out.println("StatusCose: " + response.getStatusCode());
+    }
+    @Test
+    void apiGetSingleTest(){
+        Response response = RestAssured.get("https://reqres.in/api/users/2");
+        System.out.println("Response body: " + response.getBody().asString());
+        Assert.assertEquals(response.getStatusCode(), 200);
     }
 }
